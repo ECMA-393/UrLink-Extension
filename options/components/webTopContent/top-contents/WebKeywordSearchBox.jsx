316f19f896e3934355f9493f5e72a0e2b6b22408
soeunSim <629943@naver.com>
2025-03-07 16:40:47 +0900

    refactor: select 검색창 제거

--- options/components/webTopContent/top-contents/WebKeywordSearchBox.jsx
+++ options/components/webTopContent/top-contents/WebKeywordSearchBox.jsx
@@ -1,72 +1,13 @@
 import { faMagnifyingGlass, faRotate } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 import { WebSearchContext } from "../../../context/WebSearchContext";
 
 export default function WebKeywordSearchBox({ urlNewList }) {
-  const {
-    userSelectSearchValue,
-    setUserSelectSearchValue,
-    SELECT_VALUE_STATE,
-  } = useContext(WebSearchContext);
-
   return (
     <div className="flex">
-      <WebSelectBox
-        selectData={SELECT_VALUE_STATE}
-        setUserSelectSearchValue={setUserSelectSearchValue}
-        userSelectSearchValue={userSelectSearchValue}
-      />
       <WebSearchBox urlNewList={urlNewList} />
-    </div>
-  );
-}
-
-function WebSelectBox({
-  selectData,
-  setUserSelectSearchValue,
-  userSelectSearchValue,
-}) {
-  const [isShowOptions, setIsShowOptions] = useState(false);
-
-  const handleOnChangeSelectToggle = () => {
-    return setIsShowOptions(!isShowOptions);
-  };
-
-  const handleOnChangeSelectValue = (event) => {
-    const { innerText } = event.target;
-    setUserSelectSearchValue(innerText);
-  };
-
-  return (
-    <div
-      className={
-        "relative w-[200px] h-10 px-[8px] me-3 rounded-lg bg-black cursor-pointer text-white after:content-['⌵'] after:absolute after:top-2 after:right-3"
-      }
-      onClick={() => handleOnChangeSelectToggle()}
-    >
-      <label className=" text-sm leading-10">{userSelectSearchValue}</label>
-      <div
-        className={
-          isShowOptions === false
-            ? "hidden"
-            : "block absolute overflow-hidden list-none rounded-b-lg top-[35px] left-0 w-full h-[108px] max-h-none bg-black text-white text-sm"
-        }
-      >
-        <div>
-          {selectData.map((data, index) => (
-            <button
-              className="block w-full px-[6px] py-[8px] ease-out duration-200 hover:bg-gray-600"
-              key={index}
-              value={data}
-              onClick={handleOnChangeSelectValue}
-            >
-              {data}
-            </button>
-          ))}
-        </div>
-      </div>
     </div>
   );
 }
