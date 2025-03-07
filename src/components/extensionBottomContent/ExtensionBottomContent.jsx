83ab8475b1539e9b0e9bc19d994f1fb6f83d18f1
SooBo <sjstnqh@naver.com>
2025-03-08 01:30:27 +0900

    feat: 검색 시 결과제외 나머지 부분은 스켈레톤UI 적용

--- src/components/extensionBottomContent/ExtensionBottomContent.jsx
+++ src/components/extensionBottomContent/ExtensionBottomContent.jsx
@@ -2,9 +2,7 @@ import { useContext } from "react";
 
 import ExtensionContext from "../../context/ExtensionContext";
 
-function UrlBox() {
-  const { searchBookmarkList } = useContext(ExtensionContext);
-
+function BookmarkBox({ bookmark }) {
   function faviconURL(urlOfFavicon) {
     const url = new URL(chrome.runtime.getURL("/_favicon/"));
     url.searchParams.set("pageUrl", urlOfFavicon);
@@ -13,39 +11,41 @@ function UrlBox() {
   }
 
   return (
-    <li className="h-4">
-      {searchBookmarkList.map((bookmark, index) => {
-        return (
-          <>
-            <div
-              className="p-3 bg-white hover:bg-gray-200 flex items-center"
-              key={index}
-            >
-              <a
-                className="max-w-[calc(100%-10px)] flex-grow overflow-hidden text-ellipsis whitespace-nowrap"
-                href={`${bookmark.url}`}
-                target="_blank"
-              >
-                <img
-                  className="mr-2 inline-block w-3 h-3"
-                  src={faviconURL(bookmark.url)}
-                />
-                {bookmark.title}
-              </a>
-            </div>
-            {bookmark.urlText && <hr className="ml-3 w-[95%]" />}
-            {bookmark.urlText && <HighlightKeyword bookmark={bookmark} />}
-          </>
-        );
-      })}
+    <li>
+      <div className="p-3 bg-white hover:bg-gray-200 flex items-center">
+        <a
+          className="max-w-[calc(100%-10px)] flex-grow overflow-hidden text-ellipsis whitespace-nowrap"
+          href={`${bookmark.url}`}
+          target="_blank"
+        >
+          <img
+            className="mr-2 inline-block w-3 h-3"
+            src={faviconURL(bookmark.url)}
+          />
+          {bookmark.title}
+        </a>
+      </div>
+      {bookmark.urlText && <hr className="ml-3 w-[95%]" />}
+      {bookmark.urlText && <HighlightKeyword bookmark={bookmark} />}
     </li>
   );
 }
 
-export default function ExtensionBottomContent() {
+export default function ExtensionBottomContent({ isLoading }) {
+  const { searchBookmarkList } = useContext(ExtensionContext);
+
   return (
-    <ul className="mt-[7rem] overflow-y-scroll h-[calc(100vh-7rem)]">
-      <UrlBox />
+    <ul className="mt-[7rem] overflow-y-auto h-[calc(100vh-7rem)] flex flex-col">
+      {searchBookmarkList.map((bookmark, index) => (
+        <BookmarkBox
+          key={index}
+          bookmark={bookmark}
+          index={index}
+        />
+      ))}
+      {isLoading && (
+        <div className="w-full h-full bg-gray-400 animate-pulse"></div>
+      )}
     </ul>
   );
 }
@@ -54,11 +54,7 @@ function HighlightKeyword({ bookmark }) {
   const { searchKeyword } = useContext(ExtensionContext);
 
   return (
-    <div
-      className={
-        "ml-3 mt-1 mb-2 font-normal w-full max-w-[calc(100%-0px)] overflow-hidden text-ellipsis whitespace-nowrap"
-      }
-    >
+    <div className="ml-3 mt-1 mb-2 font-normal w-full max-w-[calc(100%-0px)] overflow-hidden text-ellipsis whitespace-nowrap">
       {bookmark.urlText.split(searchKeyword).map((item, index) => {
         if (item === "") {
           return;
