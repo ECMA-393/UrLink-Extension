3ed4ed7b83f3e273987c7f4bc895cda829df951c
SooBo <sjstnqh@naver.com>
2025-03-08 00:30:31 +0900

    feat: 검색 5개씩 묶어보내기 로직 구현, 5개의 결과 먼저 표시

--- src/hooks/useFetchUrlContent.js
+++ src/hooks/useFetchUrlContent.js
@@ -12,6 +12,7 @@ const useFetchUrlContent = () => {
   const getCrawledData = useCallback(async () => {
     try {
       const bookmarkList = [...allBookmarkList];
+      let finalBookmarkList = [];
 
       if (!searchKeyword) {
         setSearchBookmarkList(allBookmarkList);
@@ -19,61 +20,62 @@ const useFetchUrlContent = () => {
       }
 
       setError(null);
+      let index = 0;
 
-      const getChunkedList = (size) => {
-        const chunkedList = [];
+      const fetchBookmarkChunk = async () => {
+        const chunkedBookmarkList = bookmarkList.slice(index, index + 5);
 
-        for (let i = 0; i < size; i++) {
-          if (bookmarkList[i]) {
-            chunkedList.push(bookmarkList[i]);
-          }
+        const fetchEncodedUrlList = chunkedBookmarkList.map((bookmark) => {
+          const encodedUrl = encodeURIComponent(bookmark.url);
 
-          bookmarkList.shift();
-        }
-
-        return chunkedList;
-      };
-
-      const chunkedBookmarkList = getChunkedList(5);
-
-      const fetchEncodedUrlList = chunkedBookmarkList.map((bookmark) => {
-        const encodedUrl = encodeURIComponent(bookmark.url);
-
-        if (searchKeyword) {
-          return fetch(
-            `${SERVER_URL}/crawl/${encodedUrl}/search?keyword=${searchKeyword}`
-          );
-        }
-      });
-
-      if (fetchEncodedUrlList) {
-        const searchResultList = await Promise.allSettled(fetchEncodedUrlList);
-        const searchedBookmarkList = [];
-
-        for (const resultList of searchResultList) {
-          if (resultList.status === "fulfilled") {
-            const searchItem = resultList.value;
-
-            if (searchItem.ok) {
-              searchedBookmarkList.push(await searchItem.json());
-            }
-          }
-        }
-
-        const filterBookmarkList = searchedBookmarkList.filter(
-          (bookmarkItem) => bookmarkItem.hasKeyword
-        );
-
-        const resultBookmarkList = filterBookmarkList.map((bookmarkItem) => {
-          for (let i = 0; i < allBookmarkList.length; i++) {
-            if (allBookmarkList[i].url === bookmarkItem.url) {
-              return { ...bookmarkItem, ...allBookmarkList[i] };
-            }
+          if (searchKeyword) {
+            return fetch(
+              `${SERVER_URL}/crawl/${encodedUrl}/search?keyword=${searchKeyword}`
+            );
           }
         });
 
-        setSearchBookmarkList(resultBookmarkList);
-      }
+        if (fetchEncodedUrlList) {
+          const searchResultList =
+            await Promise.allSettled(fetchEncodedUrlList);
+          const searchedBookmarkList = [];
+
+          for (const resultList of searchResultList) {
+            if (resultList.status === "fulfilled") {
+              const searchItem = resultList.value;
+
+              if (searchItem.ok) {
+                searchedBookmarkList.push(await searchItem.json());
+              }
+            }
+          }
+
+          const filterBookmarkList = searchedBookmarkList.filter(
+            (bookmarkItem) => bookmarkItem.hasKeyword
+          );
+
+          const resultBookmarkList = filterBookmarkList.map((bookmarkItem) => {
+            for (let i = 0; i < allBookmarkList.length; i++) {
+              if (allBookmarkList[i].url === bookmarkItem.url) {
+                return { ...bookmarkItem, ...allBookmarkList[i] };
+              }
+            }
+          });
+
+          if (resultBookmarkList) {
+            finalBookmarkList = [...finalBookmarkList, ...resultBookmarkList];
+            setSearchBookmarkList(finalBookmarkList);
+          }
+        }
+
+        index += 5;
+
+        if (index < bookmarkList.length) {
+          await fetchBookmarkChunk();
+        }
+      };
+
+      await fetchBookmarkChunk();
     } catch (error) {
       setError(error);
     } finally {
