7d01cc39541ae09c38f473fd092fa180e2e91f94
soeunSim <629943@naver.com>
2025-03-11 01:09:40 +0900

    feat: 익스텐션, 검색어 옵션페이지로 전달 할 수 있게 끔 로컬스토리지에 저장 구현 및 옵션페이지 useContext이동

--- options/App.jsx
+++ options/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 import WebContent from "./components/WebContnet";
+import { WebSearchContext } from "./context/WebSearchContext";
 
 function App() {
-  return <WebContent />;
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [urlNewList, setUrlNewList] = useState([]);
+
+  return (
+    <WebSearchContext.Provider
+      value={{
+        setUrlNewList,
+        searchKeyword,
+        setSearchKeyword,
+      }}
+    >
+      <WebContent urlNewList={urlNewList} />
+    </WebSearchContext.Provider>
+  );
 }
 
 export default App;
