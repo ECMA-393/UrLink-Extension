e2fa6f360c7e12ff84448ba519fcd1b725941ae3
seonghoon <shee5032@naver.com>
2025-03-15 01:11:22 +0900

    refactor: 검색 결과 저장 시 timestamp 값 추가하여 정렬 및 삭제 로직 개선

--- src/hooks/useFetchUrlContent.js
+++ src/hooks/useFetchUrlContent.js
@@ -122,6 +122,7 @@ const useFetchUrlContent = () => {
                   [`${bookmarkItem.url}`]: {
                     ...bookmarkItem,
                     ...allBookmarkList[i],
+                    timestamp: Date.now(),
                   },
                 });
 
